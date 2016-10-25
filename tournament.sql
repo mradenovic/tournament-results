ff7e7ab85949db5e7683428cb7161f1900581e9f
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-10-24 21:27:15 -0400

    feat(sql): create basic tables
    Create table 'players' to store player info.
    Create table 'matches' to store match info

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,22 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Connect to default data base
+\c vagrant;
 
+-- Recreate 'tournament' database
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
+
+-- Create table 'players'
+CREATE TABLE players(
+ id serial PRIMARY KEY,
+ name text
+);
+
+-- Create table 'metches'
+CREATE TABLE matches(
+ winner int references players (id),
+ loser int  references players (id)
+);
